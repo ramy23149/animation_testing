3fbd49e20bbc2a968f2825b33c6082e23992067b
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2024-03-06 21:37:50 +0200

    useing transform widget

--- lib/pages/animated_contanier.dart
+++ lib/pages/animated_contanier.dart
@@ -8,39 +8,29 @@ class AnimatedContaner extends StatefulWidget {
 }
 
 class _AnimatedContanerState extends State<AnimatedContaner> {
-  double height = 50;
-  double width = 50;
-  Color color = Colors.blue;
-  double raduis = 10;
-  double opacity = 1;
+
+  double value = 0;
+  
+  bool isVisable = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: opacity,
-          duration: const Duration(seconds: 3),
-          child: AnimatedContainer(
-            curve: Curves.easeIn,
-            duration: const Duration(seconds: 3),
-            height: height,
-            width: width,
-            decoration: BoxDecoration(
-                color: color, borderRadius: BorderRadius.circular(raduis)),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            opacity = opacity == 1 ? 0 : 1;
-            height = height == 50 ? 200 : 50;
-            width = width == 50 ? 200 : 50;
-            color = color == Colors.blue ? Colors.red : Colors.blue;
-            raduis = raduis == 10 ? 20 : 10;
-          });
-        },
-        child: const Icon(Icons.play_arrow),
+      body: Column(
+        children: [
+          Expanded(
+            child:
+             Transform.rotate(
+            angle: 22/7*value,
+              child: Image.asset('assets/images/car.jpeg'))),
+            Slider(
+              value: value,
+               onChanged: (double val)=>setState(() =>value=val),
+               min: 0,
+               max: 3,
+               divisions: 100,
+               label: value.round().toString(),
+               )
+        ],
       ),
     );
   }
